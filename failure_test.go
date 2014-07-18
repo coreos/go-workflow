19f1438e69f63252c78750b80bb21f775ca73e27
Cole Gleason <colewashere@gmail.com>
2014-07-17 17:07:15 -0700

    failure: rework InteractiveFailure to allow skipping

--- failure_test.go
+++ failure_test.go
@@ -2,6 +2,7 @@ package workflow_test
 
 import (
 	"errors"
+	"strings"
 	"testing"
 
 	"github.com/colegleason/go-workflow"
@@ -19,6 +20,38 @@ func TestFailureFunc(t *testing.T) {
 		Label: "fail workflow",
 		Run: func(c workflow.Context) error {
 			return errors.New("generic error")
+		},
+	})
+
+	err := w.Run()
+	if err != nil {
+		t.Error(err)
+	}
+	if testVar != true {
+		t.Fail()
+	}
+}
+
+func TestInteractiveFailure(t *testing.T) {
+	var testVar bool
+
+	workflow.InputFile = strings.NewReader("s\n")
+
+	w := workflow.New()
+	w.OnFailure = workflow.InteractiveFailure
+	w.AddSteps([]*workflow.Step{
+		&workflow.Step{
+			Label: "fail workflow",
+			Run: func(c workflow.Context) error {
+				return errors.New("generic error")
+			},
+		},
+		&workflow.Step{
+			Label: "succeed workflow",
+			Run: func(c workflow.Context) error {
+				testVar = true
+				return nil
+			},
 		},
 	})
 
